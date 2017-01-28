56a743929236c563188b20551d740a79502fd62f
S <null>
2017-01-27 18:52:59 -0800

    Testing

--- src/net/urbanmc/mcinfected/object/GamePlayerList.java
+++ src/net/urbanmc/mcinfected/object/GamePlayerList.java
@@ -3,7 +3,7 @@ package net.urbanmc.mcinfected.object;
 import java.util.List;
 
 public class GamePlayerList {
-
+//Hi
     private List<GamePlayer> players;
 
     public GamePlayerList(List<GamePlayer> players) {
