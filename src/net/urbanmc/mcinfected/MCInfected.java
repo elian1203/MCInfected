2467e9fa4da6ccc3d7d60e07ad7e68e9928fe7a0
elian1203 <elian1203@gmail.com>
2017-01-27 19:57:57 -0500

    Fixed createFiles() method in main class

--- src/net/urbanmc/mcinfected/MCInfected.java
+++ src/net/urbanmc/mcinfected/MCInfected.java
@@ -3,38 +3,33 @@ package net.urbanmc.mcinfected;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.IOException;
 
 public class MCInfected extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        createFiles();
-    }
+	@Override
+	public void onEnable() {
+		createFiles();
+	}
 
-    private void createFiles() {
-        getDataFolder().mkdir();
+	private void createFiles() {
+		getDataFolder().mkdir();
 
-        File file;
+		File file;
 
-        for (int i = 0; i < 4; i++) {
-            if (i == 0) {
-                file = new File(getDataFolder(), "killstreaks.yml");
-            } else if (i == 1) {
-                file = new File(getDataFolder(), "kits.yml");
-            } else if (i == 2) {
-                file = new File(getDataFolder(), "maps.yml");
-            } else {
-                file = new File(getDataFolder(), "ranks.yml");
-            }
+		for (int i = 0; i < 4; i++) {
+			if (i == 0) {
+				file = new File(getDataFolder(), "killstreaks.yml");
+			} else if (i == 1) {
+				file = new File(getDataFolder(), "kits.yml");
+			} else if (i == 2) {
+				file = new File(getDataFolder(), "maps.yml");
+			} else {
+				file = new File(getDataFolder(), "ranks.yml");
+			}
 
-            if (!file.exists()) {
-                try {
-                    file.createNewFile();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
-    }
+			if (!file.exists()) {
+				saveResource(file.getName(), false);
+			}
+		}
+	}
 }
