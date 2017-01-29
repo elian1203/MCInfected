1ca3cbbc9390aa1e00bf16f8599f7f8683f1912a
Elian <elian1203@gmail.com>
2017-01-28 21:45:45 -0500

    Finish ranks.yml

--- src/net/urbanmc/mcinfected/object/GamePlayerList.java
+++ src/net/urbanmc/mcinfected/object/GamePlayerList.java
@@ -3,7 +3,7 @@ package net.urbanmc.mcinfected.object;
 import java.util.List;
 
 public class GamePlayerList {
-//His
+
     private List<GamePlayer> players;
 
     public GamePlayerList(List<GamePlayer> players) {
