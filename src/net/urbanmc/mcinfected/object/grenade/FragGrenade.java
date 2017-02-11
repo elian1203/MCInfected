30ec06d2f4b3b4069b4eafb58c381c91f72ca2e0
S <null>
2017-02-10 19:11:54 -0800

    Updated Frag and Throwing Knife

--- src/net/urbanmc/mcinfected/object/grenade/FragGrenade.java
+++ src/net/urbanmc/mcinfected/object/grenade/FragGrenade.java
@@ -1,15 +1,27 @@
 package net.urbanmc.mcinfected.object.grenade;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class FragGrenade extends Grenade {
 
-	public FragGrenade(Item item) {
-		super(item);
-	}
+    public FragGrenade(Item item) {
+        super(item);
+    }
 
-	@Override
-	public void activate() {
+    @Override
+    public void activate() {
+        List<Entity> nearbyEntities = this.getItem().getNearbyEntities(3, 3, 3);
 
-	}
+        for (Entity entity : nearbyEntities) {
+
+            if (entity instanceof Player)
+                ((Player) entity).damage(10);
+
+        }
+
+    }
 }
--- src/net/urbanmc/mcinfected/object/grenade/ThrowingKnife.java
+++ src/net/urbanmc/mcinfected/object/grenade/ThrowingKnife.java
@@ -1,7 +1,9 @@
 package net.urbanmc.mcinfected.object.grenade;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -13,12 +15,13 @@ public class ThrowingKnife extends Grenade {
 
     @Override
     public void activate() {
+        Location loc = getItem().getLocation();
+        List<Entity> nearbyEntities = this.getItem().getNearbyEntities(1, 1, 1);
 
-        List<Entity> nearbyEntities = this.getItem().getNearbyEntities(3, 3, 3);
+        for (Entity entity : nearbyEntities) {
 
-        for(Entity entity : nearbyEntities) {
-
-
+            if (entity instanceof Player)
+                ((Player) entity).damage(16);
 
         }
     }
