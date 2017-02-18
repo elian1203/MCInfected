e5ecd8d7599d9827b337579c77d6f7f2de7821be
S <null>
2017-02-18 08:40:02 -0800

    Added disableAttackCooldown method to JoinListener

--- src/net/urbanmc/mcinfected/listener/JoinListener.java
+++ src/net/urbanmc/mcinfected/listener/JoinListener.java
@@ -3,19 +3,34 @@ package net.urbanmc.mcinfected.listener;
 import net.urbanmc.mcinfected.manager.GameManager;
 import net.urbanmc.mcinfected.manager.GamePlayerManager;
 import net.urbanmc.mcinfected.object.GamePlayer;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
 
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent e) {
-		GamePlayerManager.getInstance().register(e.getPlayer());
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        GamePlayerManager.getInstance().register(e.getPlayer());
 
-		GamePlayer p = GamePlayerManager.getInstance().getGamePlayerByUniqueId(e.getPlayer().getUniqueId());
+        GamePlayer p = GamePlayerManager.getInstance().getGamePlayerByUniqueId(e.getPlayer().getUniqueId());
 
-		GameManager.getInstance().loadPlayer(p, GameManager.getInstance().getGameState() == GameManager.GameState
-				.RUNNING);
-	}
+        GameManager.getInstance().loadPlayer(p, GameManager.getInstance().getGameState() == GameManager.GameState
+                .RUNNING);
+        disableAttackCooldown(e.getPlayer());
+    }
+
+
+    private void disableAttackCooldown(Player p) {
+        AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+
+        //At least 16 needed to disable attack cooldown
+        attribute.setBaseValue(16);
+
+        p.saveData();
+    }
+
 }
