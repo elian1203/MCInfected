4ec56125f37dea2912a2d6d5bce3ef2db10eaef7
Elian <elian1203@gmail.com>
2017-04-29 21:07:28 -0400

    Moved about message to properties file

--- src/net/urbanmc/mcinfected/command/About.java
+++ src/net/urbanmc/mcinfected/command/About.java
@@ -1,5 +1,6 @@
 package net.urbanmc.mcinfected.command;
 
+import net.urbanmc.mcinfected.manager.Messages;
 import net.urbanmc.mcinfected.object.Command;
 import net.urbanmc.mcinfected.object.GamePlayer;
 import org.bukkit.command.CommandSender;
@@ -14,11 +15,7 @@ public class About extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String label, String[] args, GamePlayer p) {
-		String message = color("&aThis plugin was developed by Elian & Silverwolfg11. We do not take any credit " +
-				                       "for any of the ideas of this plugin, only its code. Many of these ideas were" +
-				                       "developed by Supertt007. This game is based off of Infected from " +
-				                       "Call of Duty: Modern Warfare 3. For more information, " +
-				                       "visit the game wiki at http://mcinfected.wikia.com/wiki/MCInfected_Wiki");
+		String message = Messages.getInstance().getString("about");
 
 		messageSender(sender, message);
 	}
