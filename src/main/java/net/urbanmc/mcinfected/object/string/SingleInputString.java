4e73f097ebe3839cd98b3afcb17f6446263cf66f
S <null>
2020-10-30 00:03:50 -0700

    Create string input object to store replacements.

---
+++ src/main/java/net/urbanmc/mcinfected/object/string/SingleInputString.java
@@ -0,0 +1,21 @@
+package net.urbanmc.mcinfected.object.string;
+
+public class SingleInputString {
+    public final static SingleInputString EMPTY = new SingleInputString("");
+
+    private final String prefix;
+    private final String suffix;
+
+    public SingleInputString(String prefix, String suffix) {
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+
+    public SingleInputString(String prefix) {
+        this(prefix, "");
+    }
+
+    public String build(String input) {
+        return prefix + input + suffix;
+    }
+}
